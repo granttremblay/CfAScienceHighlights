c076595e0d3f6c7901bb8f17994b188679db0153
Grant Tremblay <granttremblay@gmail.com>
2025-08-30 14:56:38 -0400

    Update cfascience.py

--- cfascience.py
+++ cfascience.py
@@ -41,7 +41,7 @@ class Color:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
-    MAGENTA = '\033[35m'  # Magenta for Smithsonian authors
+    MAGENTA = '\033[35m'  # Magenta for CfA authors
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
